f8840f608fa2c56b57c049267f4dbc89d7341e54
criticalpixel <thomas@criticalpixel.com>
2018-11-05 22:52:40 +0000

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,64 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GridOptions } from 'ag-grid-community';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
 
-  columnDefs = [
-    {headerName: 'Make', field: 'make' },
-    {headerName: 'Model', field: 'model' },
-    {headerName: 'Price', field: 'price'}
-  ];
+export class AppComponent implements OnInit {
+  public rowData:any;
+  public columnDefs:any;
+  public gridOptions:GridOptions;
 
-  rowData = [
-      { make: 'Toyota', model: 'Celica', price: 35000 },
-      { make: 'Ford', model: 'Mondeo', price: 32000 },
-      { make: 'Porsche', model: 'Boxter', price: 72000 }
-  ];
+  constructor(private http: HttpClient){
+  }
+
+  ngOnInit(){
+    this.getData();
+    this.setColumnDefs();
+    this.setGridOptions();
+  }
+
+  //Fetch Data via | HTTP
+  getData(){
+    this.rowData = this.http.get('../assets/olympicWinners.json');
+  }
+
+  //Set Column Definitions
+  setColumnDefs(){
+    this.columnDefs = [
+      { field: "athlete", enableRowGroup:true, editable:true},
+      { field: "age", enableRowGroup:true},
+      { field: "country", enableRowGroup:true },
+      { field: "year", enableRowGroup:true },
+      { field: "sport", enableRowGroup:true },
+      { field: "date" },
+      { field: "gold" },
+      { field: "silver" },
+      { field: "bronze" },
+      { field: "total" }
+    ];
+
+  }
   
+
+  //Set Grid Options
+  setGridOptions(){
+    this.gridOptions = {
+      columnDefs:this.columnDefs,
+      enableSorting:true,
+      enableFilter:true,
+      floatingFilter:true,
+      rowSelection:'multiple',
+      rowMultiSelectWithClick:true,
+      showToolPanel:true,
+      rowGroupPanelShow:'always',
+      animateRows:true,
+      enableRangeSelection:true
+    }
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
+
+//GRID
+import {LicenseManager} from 'ag-grid-enterprise/main';
+import "ag-grid-enterprise";
+LicenseManager.setLicenseKey("Thomas_Horvath_EvaluationLicense_NotForProduction_999Devs14_February_2019__MTU1MDEwMjQwMDAwMA==d258abeb3508844e8f42bcddf2919d1f");
 
 @NgModule({
   declarations: [
@@ -10,6 +16,7 @@ import { AgGridModule } from 'ag-grid-angular';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AgGridModule.withComponents([])
   ],
   providers: [],
