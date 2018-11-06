e4a01f3d1f5e30c7ae2bc3d5061a53b64a27910d
Thomas Horvath <horvthecloud@gmail.com>
2018-11-06 15:06:01 +0000

    freshstart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GridOptions } from 'ag-grid-community';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,46 +9,12 @@ import { GridOptions } from 'ag-grid-community';
 export class AppComponent implements OnInit {
   public rowData:any;
   public columnDefs:any;
-  public gridOptions:GridOptions;
 
   constructor(){
   }
 
   ngOnInit(){
-    // this.getData();
-    // this.setColumnDefs();
-    // this.setGridOptions();
+   
   }
-
-  //Fetch Data via | HTTP
-  // getData(){
-  //   this.rowData = this.http.get('../assets/olympicWinners.json');
-  // }
-
-  //Set Column Definitions
-  // setColumnDefs(){
-  //   this.columnDefs = [
-  //     { field: "athlete"},
-  //     { field: "age"},
-  //     { field: "country"},
-  //     { field: "year"},
-  //     { field: "sport"},
-  //     { field: "date" },
-  //     { field: "gold" },
-  //     { field: "silver" },
-  //     { field: "bronze" },
-  //     { field: "total" }
-  //   ];
-
-  // }
-  
-
-  //Set Grid Options
-  // setGridOptions(){
-  //   this.gridOptions = {
-  //     columnDefs:this.columnDefs
-  //   }
-  // }
-
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
-
-//GRID
-import {LicenseManager} from 'ag-grid-enterprise/main';
-import "ag-grid-enterprise";
-LicenseManager.setLicenseKey("Thomas_Horvath_EvaluationLicense_NotForProduction_999Devs14_February_2019__MTU1MDEwMjQwMDAwMA==d258abeb3508844e8f42bcddf2919d1f");
 
 @NgModule({
   declarations: [
@@ -16,7 +10,6 @@ LicenseManager.setLicenseKey("Thomas_Horvath_EvaluationLicense_NotForProduction_
   ],
   imports: [
     BrowserModule,
-    //HttpClientModule,
     AgGridModule.withComponents([])
   ],
   providers: [],
